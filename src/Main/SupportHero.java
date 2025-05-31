de9864b0504a43b2be9481ec00ce66f761ab356d
Clarence <quacky888@gmail.com>
2025-05-31 13:16:46 +0700

    Update SupportHero.java

--- src/Main/SupportHero.java
+++ src/Main/SupportHero.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 class SupportHero extends Hero {
     public SupportHero(String name) {
-        super(name, 100, 10, 15, 25);
+        super(name, 100, 10, 15, 25, 10, 20);
     }
 
     public void useSkill(List<Hero> team, List<Monster> enemies) {
         System.out.println(name + " buffs attack of all Hunt/Destruction heroes by 10");
         for (Hero h : team)
-            if (h instanceof HuntHero || h instanceof DestructionHero) h.attack += 10;
+            if (h instanceof HuntHero || h instanceof DestructionHero) h.attack += skillVar;
     }
 
     public void useUltimate(List<Hero> team, List<Monster> enemies) {
         System.out.println(name + " greatly buffs attack of all Hunt/Destruction heroes by 20");
         for (Hero h : team)
-            if (h instanceof HuntHero || h instanceof DestructionHero) h.attack += 20;
+            if (h instanceof HuntHero || h instanceof DestructionHero) h.attack += ultVar;
     }
 }
