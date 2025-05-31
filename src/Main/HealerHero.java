b290cebcbc3552f7ddeeeabf03cb606686d4f54e
Clarence <quacky888@gmail.com>
2025-05-31 13:15:31 +0700

    Update HealerHero.java

--- src/Main/HealerHero.java
+++ src/Main/HealerHero.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 class HealerHero extends Hero {
     public HealerHero(String name) {
-        super(name, 90, 10, 10, 25);
+        super(name, 90, 10, 10, 25, 30, 50);
     }
 
     public void useSkill(List<Hero> team, List<Monster> enemies) {
         Hero target = selectAlly(team);
         if (target != null) {
             System.out.println(name + " heals " + target.name + " for 30 HP.");
-            target.heal(30);
+            target.heal(skillVar);
         }
     }
 
     public void useUltimate(List<Hero> team, List<Monster> enemies) {
         System.out.println(name + " uses Full Recovery!");
-        for (Hero h : team) h.heal(50);
+        for (Hero h : team) h.heal(ultVar);
     }
 }
