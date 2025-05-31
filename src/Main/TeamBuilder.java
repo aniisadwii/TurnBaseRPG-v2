caa2889cb69eda25a9e71e2dc75d97c0a36ad2de
ve-ltn <litanayuevelyn@gmail.com>
2025-05-31 12:05:40 +0700

    Message chain on TeamBuilder in chooseTeam()
    Code smell's name: Message chain.
    
    Reason: There’s a line of code that contains more than 3 chained calls to methods and properties of another object "pool.get(i).getClass().getSimpleName()".
    
    Refactor method: Extract method.

--- src/Main/TeamBuilder.java
+++ src/Main/TeamBuilder.java
@@ -2,12 +2,17 @@ package Main;
 import java.util.*;
 
 class TeamBuilder {
+	private String getHeroType(Hero hero) {
+        return hero.getClass().getSimpleName();
+    }
+	
     public List<Hero> chooseTeam(List<Hero> pool, Scanner sc) {
         List<Hero> team = new ArrayList<>();
         while (team.size() < 5) {
             System.out.println("Select hero " + (team.size() + 1) + "/5:");
             for (int i = 0; i < pool.size(); i++) {
-                System.out.println((i + 1) + ". " + pool.get(i).name + " [" + pool.get(i).getClass().getSimpleName() + "]");
+                Hero currentHero = pool.get(i);
+                System.out.println((i + 1) + ". " + currentHero.name + " [" + getHeroType(currentHero) + "]");
             }
             int choice = sc.nextInt() - 1;
             if (choice >= 0 && choice < pool.size()) {
