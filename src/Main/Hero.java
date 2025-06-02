f41fb47e84e41070abc636563f9ea0f425a45857
DanielHyoryno <168710140+DanielHyoryno@users.noreply.github.com>
2025-06-02 15:41:53 +0700

    Update Hero.java

--- src/Main/Hero.java
+++ src/Main/Hero.java
@@ -3,19 +3,21 @@ import java.util.*;
 
 abstract class Hero {
     String name;
-    int hp, maxHp, attack, defense, speed;
+    int hp, maxHp, attack, defense, speed, skillVar, ultVar;
     double ultimateGauge = 0.0;
     int turnCount = 0;
     static int teamSkillPoints = 5;
     Scanner sc = new Scanner(System.in);
 
-    public Hero(String name, int hp, int attack, int defense, int speed) {
+    public Hero(String name, int hp, int attack, int defense, int speed, int skillVar, int ultVar) {
         this.name = name;
         this.hp = hp;
         this.maxHp = hp;
         this.attack = attack;
         this.defense = defense;
         this.speed = speed;
+        this.skillVar = skillVar;
+        this.ultVar = ultVar;
     }
 
     public boolean isAlive() {
