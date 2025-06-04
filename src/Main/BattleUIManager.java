6228a468250ec439c70dfd74615aaf9968879e70
Clarence <quacky888@gmail.com>
2025-06-04 20:47:11 +0700

    Update BattleUIManager.java

--- src/Main/BattleUIManager.java
+++ src/Main/BattleUIManager.java
@@ -8,7 +8,7 @@ public class BattleUIManager {
 
     public static String getHeroInput(Hero hero, int level) {
         String ultStatus = (hero.turnCount % 3 == 0 && hero.turnCount != 0) ? "READY" : "NOT READY";
-        System.out.println("\n[Wave " + level + "] " + hero.name + "'s turn [HP: " + hero.hp + "/" + hero.maxHp + ", ULT: " + ultStatus + "]");
+        System.out.println("\n[Wave " + level + "] " + hero.name + "'s turn [HP: " + hero.stats.hp + "/" + hero.stats.maxHp + ", ULT: " + ultStatus + "]");
         System.out.println("Enter 's' to skip this turn, 'x' to end the game, or any other key to continue:");
         return sc.nextLine();
     }
