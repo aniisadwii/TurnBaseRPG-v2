513fbdd31b2b00455ab9745b5c429372b2206382
Clarence <quacky888@gmail.com>
2025-06-04 20:46:28 +0700

    Update PreservationHero.java

--- src/Main/PreservationHero.java
+++ src/Main/PreservationHero.java
@@ -8,11 +8,11 @@ class PreservationHero extends Hero {
 
     public void useSkill(List<Hero> team, List<Monster> enemies) {
         System.out.println(name + " uses Shield Wall: +10 DEF to all allies");
-        for (Hero h : team) h.defense += 10;
+        for (Hero h : team) h.stats.defense += 10;
     }
 
     public void useUltimate(List<Hero> team, List<Monster> enemies) {
         System.out.println(name + " uses Fortress Aura: +20 DEF to all allies");
-        for (Hero h : team) h.defense += 20;
+        for (Hero h : team) h.stats.defense += 20;
     }
 }
