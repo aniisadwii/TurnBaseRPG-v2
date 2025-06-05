e042525f6cd64730cfee35cc80df7d894da20b62
DanielHyoryno <168710140+DanielHyoryno@users.noreply.github.com>
2025-06-05 14:45:26 +0700

    Update BattleManager.java

--- src/Main/BattleManager.java
+++ src/Main/BattleManager.java
@@ -4,6 +4,17 @@ package Main;
 import java.util.*;
 
 public class BattleManager {
+
+    public void handleBattle(TeamBuilder teamBuilder, HeroManager manager, Scanner sc) {
+        List<Hero> team = teamBuilder.chooseTeam(manager.getHeroList(), sc);
+        for (int lvl = 1; lvl <= 20; lvl++) {
+            System.out.println("\n-- LEVEL " + lvl + " --");
+            startBattle(team, lvl);
+            if (team.stream().noneMatch(Hero::isAlive)) break;
+            for (Hero h : team) h.restoreFullHealth();
+        }
+    }
+
     public void startBattle(List<Hero> team, int level) {
         List<Monster> monsters = MonsterFactory.createMonsters(level);
         TeamSkillPointManager.resetTeamSkillPoints();
