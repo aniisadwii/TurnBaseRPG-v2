a770e4685c109a6d5f3e4e8da4c6ed1a3e9297d1
DanielHyoryno <168710140+DanielHyoryno@users.noreply.github.com>
2025-06-05 21:51:06 +0700

    Update HeroType.java

--- src/Main/HeroType.java
+++ src/Main/HeroType.java
@@ -4,14 +4,7 @@ public enum HeroType {
     HUNT, DESTRUCTION, PRESERVATION, HEALER, SUPPORT;
 
     public static HeroType fromInt(int i) {
-    	switch (i) {
-	        case 1: return HUNT;
-	        case 2: return DESTRUCTION;
-	        case 3: return PRESERVATION;
-	        case 4: return HEALER;
-	        case 5: return SUPPORT;
-	        default: return null;
-    	}
-
+        HeroType[] values = HeroType.values();
+        return (i >= 1 && i <= values.length) ? values[i - 1] : null;
     }
 }
